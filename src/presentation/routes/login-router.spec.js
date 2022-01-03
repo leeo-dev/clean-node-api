5fd2a0b095969858140e26e2bda3223b91019813
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-01-02 21:25:17 -0300

    ensure login router return status code 400 if no email is provided

---
+++ src/presentation/routes/login-router.spec.js
@@ -0,0 +1,23 @@
+// SUT - system under test
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
