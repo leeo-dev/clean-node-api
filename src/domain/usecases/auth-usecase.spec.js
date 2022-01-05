9a1f94e1113be011f97b185345a8a610492a7117
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-01-05 20:04:51 -0300

    test: ensure AuthUseCase throws if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,15 @@
+const { describe, test, expect } = require('@jest/globals')
+class AuthUseCase {
+  async auth (email) {
+    if (!email) {
+      throw new Error()
+    }
+  }
+}
+describe('Auth UseCase', () => {
+  test('Should throw if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})
