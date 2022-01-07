2f91bdf9f1fd1d0af3aef8a397b4e31b5f2bcd05
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-01-07 20:52:30 -0300

    ensure TokenGenerator returns null if JWT returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,15 @@
+const { expect, describe, test } = require('@jest/globals')
+
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+
+describe('TokenGenerator', () => {
+  test('should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
