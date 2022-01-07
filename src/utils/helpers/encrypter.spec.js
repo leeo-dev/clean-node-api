909bc2d0eb6d97fbba6ddf661e843fee454949f8
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-01-07 20:14:38 -0300

    test: ensure Encrypter returns true if bcrypt returns true

---
+++ src/utils/helpers/encrypter.spec.js
@@ -0,0 +1,15 @@
+const { expect, describe, test } = require('@jest/globals')
+
+class Encrypter {
+  compare (password, hashedPassword) {
+    return true
+  }
+}
+
+describe('Encrypter', () => {
+  test('Should return true if bcrypt returns true', async () => {
+    const sut = new Encrypter()
+    const isValid = await sut.compare('any_password', 'hashed_password')
+    expect(isValid).toBe(true)
+  })
+})
